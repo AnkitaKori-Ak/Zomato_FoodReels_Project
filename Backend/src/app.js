270e953367d734a8aa403325c92c05f56905a448
AnkitaKori-Ak <koriankita.01@gmail.com>
2025-12-08 17:10:39 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,15 @@ app.use(cors({
     origin:"https://zomato-food-reels-project.vercel.app",
     credentials:true
 }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 
